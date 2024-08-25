f941407467cd6365e238215d9d4284dafe122b05
Kennedy821 <57705754+Kennedy821@users.noreply.github.com>
2024-08-25 14:39:51 +0100

    Add files via upload

--- pages/research_app_gcs.py
+++ pages/research_app_gcs.py
@@ -63,6 +63,14 @@ import json
 # from fuzzywuzzy import fuzz
 # from fuzzywuzzy import process
 
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
 im = Image.open('slug_logo.png')
 st.set_page_config(
     page_title="PSIL",
