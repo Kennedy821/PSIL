972159504da8fa7ce6f9cca6924e32c8b676b44b
Kennedy821 <57705754+Kennedy821@users.noreply.github.com>
2024-09-04 12:50:14 +0100

    Add files via upload

---
+++ pages/psil_login.py
@@ -0,0 +1,114 @@
+import streamlit as st
+import jwt  # To generate and decode tokens
+import bcrypt
+from google.oauth2 import service_account
+from PIL import Image
+from google.cloud import storage
+import pandas as pd
+import time
+import tempfile
+print(jwt.__version__)
+SECRET_KEY = "your_secret_key"  # A secret key for encoding and decoding tokens
+
+
+def get_login_credentials_for_valid_user():
+    while True:
+
+
+        #download the indices from gcs
+        blob = bucket.blob("login_url_data.csv")
+        if blob.exists():
+
+            # Download the file to a destination
+            blob.download_to_filename(temp_dir+"login_url_data.csv")
+            login_url = pd.read_csv(temp_dir+"login_url_data.csv").values[0]
+            return login_url
+
+            break
+        else:
+            time.sleep(5)
+
+
+# Simulate user authentication
+def authenticate_user(email, password):
+    if email == "test@test.com" and password == "password123":
+        return True
+    return False
+
+# Generate JWT token after login
+def generate_token(email):
+    token = jwt.encode({"email": email}, SECRET_KEY, algorithm="HS256")
+    return token
+
+# Decode and verify JWT token
+def verify_token(token):
+    try:
+        decoded_token = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        return decoded_token
+    except jwt.ExpiredSignatureError:
+        return None  # Token expired
+    except jwt.InvalidTokenError:
+        return None  # Invalid token
+# Hash the password
+def hash_password(plain_text_password):
+    # Generate a salt
+    salt = bcrypt.gensalt()
+    # Hash the password with the salt
+    hashed_password = bcrypt.hashpw(plain_text_password.encode('utf-8'), salt)
+    return hashed_password
+    
+def check_password(stored_hashed_password, plain_text_password):
+    # Check the provided password against the stored hashed password
+    return bcrypt.checkpw(plain_text_password.encode('utf-8'), stored_hashed_password)
+
+# Login form
+st.title("Login")
+email = st.text_input("Email")
+password = st.text_input("Password", type="password")
+
+if st.button("Login"):
+    if authenticate_user(email, password):
+        with tempfile.TemporaryDirectory() as temp_dir:
+
+            credentials_df = pd.DataFrame([email,password]).T
+            credentials_df.columns = ["email","pw"]
+            credentials_df["hash_pw"] = credentials_df.pw.apply(lambda x: hash_password(x))
+            credentials_df.drop(columns="pw", inplace=True)
+
+            # Create credentials object
+            credentials = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"])
+
+            # Use the credentials to create a client
+            client = storage.Client(credentials=credentials)
+
+
+            # The bucket on GCS in which to write the CSV file
+            bucket = client.bucket('psil-app-backend-2')
+            # The name assigned to the CSV file on GCS
+            blob = bucket.blob('user_login_request.csv')
+
+            # Convert the DataFrame to a CSV string with a specified encoding
+            csv_string = credentials_df.to_csv(index=False, encoding='utf-8')
+
+            # Upload the CSV string to GCS
+            blob.upload_from_string(csv_string, 'text/csv')
+
+
+            # wait for the valid url to appear in the bucket
+            redirect_url = []
+            get_login_credentials_for_valid_user()
+            redirect_url = redirect_url[0]
+
+
+
+
+            # token = generate_token(email)  # Generate token after login
+            st.success("Login successful!")
+            
+            # # Redirect to external site with the token as a query parameter
+            # redirect_url = f"https://psilproject.streamlit.app/research_app_gcs_login?token={token}"
+            st.markdown(f"""
+            <meta http-equiv="refresh" content="0; url={redirect_url}">
+            """, unsafe_allow_html=True)
+    else:
+        st.error("Invalid email or password")
