f2ee01911d5e12b619c113d7e864dbb8dfe7d7d4
tariroMashCasa <ucfntma@ucl.ac.uk>
2024-12-06 10:19:25 +0000

    Add files via upload

--- pages/research_app_gcs_login_new.py
+++ pages/research_app_gcs_login_new.py
@@ -757,14 +757,12 @@ if 'token' in query_params:
                         unsafe_allow_html=True
                     )
 
-                    def change_label_style(label, font_size='12px', font_color='black', font_family='sans-serif'):
+                    def change_label_style(label, font_size='12px'):
                         html = f"""
                         <script>
                             var elems = window.parent.document.querySelectorAll('p');
                             var elem = Array.from(elems).find(x => x.innerText == '{label}');
                             elem.style.fontSize = '{font_size}';
-                            elem.style.color = '{font_color}';
-                            elem.style.fontFamily = '{font_family}';
                         </script>
                         """
                         st.components.v1.html(html)
