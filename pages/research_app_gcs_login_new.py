6da8f00eb84d027726650641c0d53021eee68d92
Kennedy821 <57705754+Kennedy821@users.noreply.github.com>
2024-12-16 13:16:58 +0000

    Add files via upload

--- pages/research_app_gcs_login_new.py
+++ pages/research_app_gcs_login_new.py
@@ -544,9 +544,9 @@ def get_previous_recommendations_fast(chosen_user):
 
             # Extract and parse the recommendation date from the file name
             recommendation_date_str = blob.name.split("/")[-1].split("_psil")[0]
-            st.write(recommendation_date_str)
+            # st.write(recommendation_date_str)
             try:
-                recommendation_date = pd.to_datetime(recommendation_date_str)
+                recommendation_date = pd.to_datetime(recommendation_date_str, format="%d_%m_%Y")
             except ValueError:
                 recommendation_date = None  # Handle parsing errors if necessary
 
